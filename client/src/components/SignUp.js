1385429494441ad4ac936f4be5c28b3af6e2697a
DJ Lancaster <djlancaster443@gmail.com>
2020-10-12 20:15:29 -0500

    added functionality to the sign up form

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -3,7 +3,8 @@ import isEmpty from 'validator/lib/isEmpty';
 import isEmail from 'validator/lib/isEmail';
 import equals from 'validator/lib/equals';
 import {Link} from 'react-router-dom';
-import {showErrorMsg} from '../Helpers/message';
+import {showErrorMsg, showSuccessMsg} from '../Helpers/message';
+import {showLoading} from '../Helpers/loading';
 import './Signup.css';
 
 const SignUp = () => {
@@ -24,6 +25,8 @@ const SignUp = () => {
         setFormData({
             ...formData,
             [evt.target.name]: evt.target.value,
+            successMsg: '',
+            errorMsg: '',
         });
     }
 
@@ -45,12 +48,15 @@ const SignUp = () => {
             })
         } else {
             //Success Msg
+            setFormData({
+                ...formData, successMsg: 'Validation Success'
+            })
         }
     }
 
     /* VIEWS */
     const showSignUpForm = () => (
-        <form className='signup-form' onSubmit={handleSubmit}>
+        <form className='signup-form' onSubmit={handleSubmit} noValidate>
             {/* usernamej*/}
             <div className='form-group input-group'>
                 <div clssName='input-group-prepend'>
@@ -133,9 +139,11 @@ const SignUp = () => {
         <div className='signup-container'>
             <div className='row px-3 vh-100'>
                 <div className='col-md-5 mx-auto align-self-center'>
-                    {showSignUpForm()}
+                    {successMsg && showSuccessMsg(successMsg)}
                     {errorMsg && showErrorMsg(errorMsg)}
-                    {JSON.stringify(formData)}
+                    {loading && <div className="text-center pb-4">{showLoading()}</div>}
+                    {showSignUpForm()}
+                    {/*JSON.stringify(formData)*/}
                 </div>
             </div>
         </div>
